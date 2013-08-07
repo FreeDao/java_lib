5d4c1186b3bd98ec2083f2c0b7e5fc73aec1ca9f
Rock Zang <rzang@airbiquity.com>
2013-08-07 09:41:04 +0800

    08.07

--- CountService2/src/com/sam/countservice2/CountService2Activity.java
+++ CountService2/src/com/sam/countservice2/CountService2Activity.java
@@ -1,8 +1,8 @@
 package com.sam.countservice2;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
 
 public class CountService2Activity extends Activity {
 
@@ -10,11 +10,11 @@ public class CountService2Activity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_count_service2);
-		startService(new Intent(this,MyService.class));//主界面阻塞，最终会出现Application not responding
+		startService(new Intent(this,MyIntentService.class));//主界面阻塞，最终会出现Application not responding
         //连续两次启动IntentService，会发现应用程序不会阻塞，而且最重的是第二次的请求会再第一个请求结束之后运行(这个证实了IntentService采用单独的线程每次只从队列中拿出一个请求进行处理)
         startService(new Intent(this,MyIntentService.class));
         startService(new Intent(this,MyIntentService.class));
 
 
 }
-』
+}
